40a09e3dfe40e549922c38e361f2f60186677bb1
ENESCHNGR <113687747+ENESCHNGR@users.noreply.github.com>
2022-09-28 19:18:08 +0300

    miniProjeAsalSayi

---
+++ src/miniProjeAsalSayi/Main12.java
@@ -0,0 +1,38 @@
+package miniProjeAsalSayi;
+
+public class Main12 {
+
+	public static void main(String[] args) {
+      int number = 1;
+      int remainder = number % 2;
+      //System.out.println(remainder);
+      boolean isPrime = true;
+      
+      if (number==1) {
+    	  System.out.println("Sayı Asal değildir");
+    	  return;
+      }
+      
+      if (number<1) {
+    	  System.out.println("Geçersiz sayi");
+      }
+    	  
+     
+      for (int i=2; i<number; i++) {
+    	  if (number % i == 0) {
+    		  isPrime = false;
+    	  }
+    		  
+      }
+      if(isPrime) {
+    	  System.out.println("Sayı Asaldır");
+      } else {
+    	  System.out.println("Sayı Asal Değildir");
+
+    	  }
+    	
+
+      }
+	}
+
+
